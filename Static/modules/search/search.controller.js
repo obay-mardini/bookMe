6af4e9e8d8862a1416151c91033f2105e05f7c65
obay <obaymardini@gmail.com>
2016-10-12 13:01:15 +0200

    get the data successfully from skyscanner api

--- Static/modules/search/search.controller.js
+++ Static/modules/search/search.controller.js
@@ -5,8 +5,28 @@
         .module('app.search')
         .controller('SearchController', SearchController);
     
-    function SearchController() {
+    SearchController.$inject = ["$http"]
+    function SearchController($http) {
         var vm = this;
+        vm.flights;
+        $http.defaults.headers.common = {
+                'Access-Control-Allow-Origin': '*'
+        };
         
+        
+        vm.search = search;
+        search();
+        
+        function search(data) {
+            $http.post("/search").then(function(result){
+                vm.flights =  result.data.Itineraries || vm.flights;
+                console.log(vm.flights)
+                vm.flights.forEach(function(flight){
+                                   console.log(flight.PricingOptions[0].Price);
+                                   });
+            }).catch(function(err){
+                console.log(err)
+            });
+        }
     }
 })();
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var http = require('https');
 var app = express();
 var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var querystring = require('querystring');
+var url = require('url');
 var dbUrl = process.env.DATABASE_URL || "postgres://spiced:spiced1@localhost:5432/encounter";
 
 app.use(express.static(__dirname + '/Static'));
@@ -33,6 +36,160 @@ app.get('/isLoggedIn',isLoggedIn, function(req,res){
    res.json({name: req.session.user.user,
             isLoggedIn: req.session.user.loggedin
             })
+});
+
+// sky scanner api
+/* first way
+app.post('/search', function(req, res, next){
+    console.log('here')
+    var postData = querystring.stringify({country:'UK',
+                currency:'GBP',
+                locale:'en-GB',
+                locationSchema:'iata',
+                apiKey:'prtl6749387986743898559646983194',
+                grouppricing:'on',
+                originplace:'EDI',
+                destinationplace:'LHR',
+                outbounddate:'2016-10-17',
+                inbounddate:'2016-10-24',
+                adults:1,
+                children:0,
+                infants:0,
+                cabinclass:'Economy'
+                });
+    console.log(postData)
+    var options = {
+        host: 'api.skyscanner.net',
+        path: '/apiservices/pricing/v1.0?apikey=prtl6749387986743898559646983194',
+        method: 'POST',
+        headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept': 'application/json',
+                'Content-Length': Buffer.byteLength(postData)
+             }
+        };
+    var request = http.request(options, function(response){
+        var time1 = new Date;
+        console.log(response.headers)
+        console.log('anything')
+        response.on('data',function(){
+            console.log('data')
+        })
+          response.on('end', function () {
+            var options = {
+              host: 'api.skyscanner.net',
+              path: '/apiservices/pricing/v1.0/'+ response.headers.location.split('v1.0/')[1] + '?apikey=prtl6749387986743898559646983194',
+              method: 'GET'
+            };
+              //https://api.skyscanner.net/apiservices/pricing/uk1/v1.0/6db32d9e6b5f42c9bb6f5588045a702b_ecilpojl_A6BDC21B53CE87D447B4556298C6028A?apikey=prtl6749387986743898559646983194
+            console.log(options.host.concat(options.path) )
+            var request2 = http.request(options,function(response){
+                var str = '';
+              
+                response.on('data',function(chunk){
+                    str += chunk        
+                });
+                response.on('end', function(){
+                    console.log(0.001*(new Date - time1));
+//var parsedResult = JSON.parse(str)
+                    res.end(str);
+                });
+            })
+            function sendIt() {
+                console.log('send it')
+                request2.end();
+            }
+            setTimeout(sendIt,150);
+            request2.on('error',function(error){
+                console.log(error)
+            })
+          });
+        request.on('error', function(err){
+            console.log(err);
+        });
+    });
+    
+    request.write(postData);
+    request.end();
+    
+})
+*/
+
+//second way
+app.post('/search', function(req, res, next) {
+    var data = querystring.stringify({country:'UK',
+                currency:'USD',
+                locale:'en-GB',
+                locationSchema:'iata',
+                apiKey:'prtl6749387986743898559646983194',
+                grouppricing:'on',
+                originplace:'BKK',
+                destinationplace:'LHR',
+                outbounddate:'2016-10-17',
+                inbounddate:'2016-10-24',
+                adults:1,
+                children:0,
+                infants:0,
+                cabinclass:'Economy'
+                });
+    
+     var options = {
+        host: 'api.skyscanner.net',
+        path: '/apiservices/pricing/v1.0?apikey=prtl6749387986743898559646983194',
+        method: 'POST',
+        headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept': 'application/json',
+                'Content-Length': Buffer.byteLength(data)
+             }
+        };
+    
+    var request = http.request(options, function(response){
+        
+        req.session.key = response.headers.location;
+        response.on('data',function(){
+            //
+        });
+        
+        response.on('end', function(){
+           console.log('end')
+            res.redirect('/pollSession');
+        });
+        
+    });
+    request.on('error', function(err){
+        res.status(500);
+        res.end('server error please try again later');
+    });
+    request.write(data);
+    request.end()
+});
+
+app.get('/pollSession', function(req, res, next){
+    var target = url.parse(req.session.key);
+    var options = {
+        host: target.host,
+        path: target.path + '?apikey=prtl6749387986743898559646983194',
+        method: 'GET'
+    };
+    
+    var request = http.request(options, function(response){
+        console.log(response.statusCode);
+       var str = '';
+       var count = 1;
+       response.on('data', function(chunk){
+           str += chunk;
+       });
+        
+        response.on('end', function(){
+            res.end(str)
+        });
+    });
+    request.on('error', function(err){
+        res.status(500);
+        res.end('server error please try again later');
+    });
+    request.end();
 });
 
 //comments
@@ -105,9 +262,9 @@ app.post("/register", function(req,res) {
                 res.end();
             }
             
-        })
-    })
-})
+        });
+    });
+});
 
 
 app.post("/login", function(req,res) {
