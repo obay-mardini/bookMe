695a4214905fdbb20e4693b10884717b1b84333f
obay <obaymardini@gmail.com>
2016-11-15 00:45:43 +0100

    modify auth module to use pcrypt

--- auth.js
+++ auth.js
@@ -1,43 +1,72 @@
 var queryDb = require('./queryDb');
 var pool = queryDb.pool;
+var crypt = require('./crypting.js');
 
 //register
 function register(query, body, callback) {
-    pool.connect(function(err, client, done) {
-        if (err) {
-            return console.error('error fetching client from pool', err);
-        }
-
-        client.query(query, [body.user, body.email, body.password, body.country], function(err, result) {
+    crypt.hashPassword(body.password).then(function(password) {
+        pool.connect(function(err, client, done) {
             if (err) {
-                callback(err)
-
-            } else {
-                callback(null, result.rows);
-                done();
+                return console.error('error fetching client from pool', err);
             }
+
+            client.query(query, [body.user, body.email, password, body.country], function(err, result) {
+                if (err) {
+                    callback(err)
+
+                } else {
+                    callback(null, result.rows);
+                    done();
+                }
+            });
         });
-    });
+    })
+
 }
 
-function login(query, body, callback) {
+function login(body, callback) {
+    checkUserAuth(body.email, body.password, function(err, result) {
+        if (err) {
+            callback('the email or password is not correct');
+        } else {
+            crypt.checkPassword(body.password, result.rows[0].password, function(err, doesMatch) {
+                if (err) {
+                    callback(err);
+                }
+
+                if (doesMatch) {
+                    callback(null, result);
+                } else {
+                    callback('the email or password is not correct');
+                }
+            });
+        }
+    });
+
+}
+
+function checkUserAuth(email, password, callback) {
+    var query = 'select * from users where email = $1';
+
     pool.connect(function(err, client, done) {
         if (err) {
             return console.error('error fetching client from pool', err);
         }
 
-        client.query(query, [body.email, body.password], function(err, result) {
-            if (err) {
-                callback(err);
-            } else if (result.rows.length === 0) {
-                callback('the email or password is not correct')
+        client.query(query, [email], function(err, result) {
+            if (!result.rows[0]) {
+                callback('err');
+                done();
             } else {
                 callback(null, result);
                 done();
             }
         });
+
     });
+
 }
+
 
 exports.register = register;
 exports.login = login;
