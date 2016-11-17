4b02dc719fd4b3b2de01c18243604e15c1b2f309
obay <obaymardini@gmail.com>
2016-11-17 22:57:04 +0100

    add current page to the service object and clean the code

--- Static/modules/getFlight/getFlights.service.js
+++ Static/modules/getFlight/getFlights.service.js
@@ -10,24 +10,27 @@
     function getFlights($http, $q, $routeParams) {
         var service = {
             search: search,
-            error: 'null',
             flights: {},
             orderNextPage: orderNextPage,
             orderPreviousPage: orderPreviousPage,
-            goToPage: goToPage,
             errors: {},
             journeyId: null,
             loading: false,
-            currency: 'U'
+            currency: 'U',
+            page: currentParam
         };
 
         return service;
 
         ////////////
         var data = [];
+        
+        function currentParam() {
+            return parseInt($routeParams.id, 10);
+        }
 
         function orderNextPage() {
-            var page = parseInt($routeParams.id, 10) + 1;
+            var page = service.page() + 1;
             service.loading = true;
             page >= 0 ? page = page : page = 0;
             $http.get("/pollSession/" + page).then(setFlights).then(function() {
@@ -47,23 +50,9 @@
         }
 
         function orderPreviousPage() {
-            var page = parseInt($routeParams.id, 10) - 1;
+            var page = service.page() - 1;
             service.loading = true;
             $http.get("/pollSession/" + page).then(setFlights).then(function() {
-                location.hash = 'search/' + page;
-            }).catch(function(err) {
-                console.log(err);
-                service.errors[err.data] = err.data;
-                service.loading = false;
-            })
-        }
-
-        function goToPage(page) {
-            var start = new Date;
-            page >= 0 ? page = page : page = 0;
-            service.loading = true;
-            $http.get("/pollSession/" + page).then(setFlights).then(function() {
-                console.log(new Date - start)
                 location.hash = 'search/' + page;
             }).catch(function(err) {
                 console.log(err);
@@ -106,139 +95,61 @@
 
         }
 
+        function sortMainParameters(result) {
+            var mainObjects = {};
+            var agents = {};
+            var legs = {};
+            var places = {};
+            var carriers = {};
+
+            result.data.Agents.forEach(function(agent) {
+                agents[agent.Id.toString()] = agent.Name;
+            })
+            mainObjects.agents = agents;
+
+            result.data.Legs.forEach(function(leg) {
+                legs[leg.Id] = [leg.Arrival, leg.Departure, leg.Directionality, leg.DestinationStation, leg.OriginStation, leg.Carriers, leg.Stops, leg.Duration];
+            });
+            mainObjects.legs = legs;
+
+            result.data.Places.forEach(function(place) {
+                places[place.Id] = place.Code;
+            });
+            mainObjects.places = places;
+
+            result.data.Carriers.forEach(function(carrier) {
+                carriers[carrier.Id] = [carrier.ImageUrl, carrier.Name];
+            });
+            mainObjects.carriers = carriers;
+
+            return mainObjects;
+        }
+
         function setFlights(result) {
             if (result.data.Status === "UpdatesComplete" && result.data.Itineraries.length === 0) {
                 service.errors.dates = 'No results found, please try another dates!!';
                 return;
             }
+
             if (result.data.Status === "UpdatesPending") {
-                console.log('poll again')
                 return setTimeout(function() {
                     return search(data)
                 }, 1000);
             }
-            var agents = {};
-            result.data.Agents.forEach(function(agent) {
-                agents[agent.Id.toString()] = agent.Name;
-            })
-            var legs = {};
-            result.data.Legs.forEach(function(leg) {
-                legs[leg.Id] = [leg.Arrival, leg.Departure, leg.Directionality, leg.DestinationStation, leg.OriginStation, leg.Carriers, leg.Stops, leg.Duration];
-            });
-            var places = {};
-            result.data.Places.forEach(function(place) {
-                places[place.Id] = place.Code;
-            });
 
-            var carriers = {};
-            result.data.Carriers.forEach(function(carrier) {
-                carriers[carrier.Id] = [carrier.ImageUrl, carrier.Name];
-            })
+            var mainObject = sortMainParameters(result);
             var id = result.config.url.split('/')[2] || 0;
-
+            var nowDate = new Date;
             service.flights = service.flights || [];
             service.flights[id] = result.data.Itineraries || service.flights[id];
             service.flights[id] = service.flights[id].map(function(flight, index) {
-                var outboundId = flight.OutboundLegId;
-                var inboundId = flight.InboundLegId;
-                var arrival = legs[outboundId][0];
-                var departure = legs[outboundId][1];
-                var arrivalDate = legs[outboundId][0].split('T')[0];
-                var arrivalTime = legs[outboundId][0].split('T')[1].slice(0, 5);
-                var departureDate = legs[outboundId][1].split('T')[0];
-                var departureTime = legs[outboundId][1].split('T')[1].slice(0, 5);
-                var arrivalR = inboundId && legs[inboundId][0];
-                var departureR = inboundId && legs[inboundId][1];
-
-                var arrivalRDate = legs[inboundId][0].split('T')[0];
-                var arrivalRTime = inboundId && legs[inboundId][0].split('T')[1].slice(0, 5);
-                var departureRDate = inboundId && legs[inboundId][1].split('T')[0];
-                var departureRTime = inboundId && legs[inboundId][1].split('T')[1].slice(0, 5);
-                var destination = legs[outboundId][3];
-                var origin = legs[outboundId][4];
-                try {
-                    var carrierImage = carriers[legs[outboundId][5]][0];
-                    var carrierImageR = carriers[legs[inboundId][5]][0];
-                    var carrierName = carriers[legs[outboundId][5]][1];
-                    var carrierNameR = carriers[legs[inboundId][5]][1];
-                } catch (err) {
-                    var carrierImage = 'http://s1.apideeplink.com/images/airlines/AA.png';
-                    var carrierImageR = 'http://s1.apideeplink.com/images/airlines/AA.png';
-                    var carrierName = 'American Airlines';
-                    var carrierNameR = 'American Airlines';
-                }
-                try {
-                    var carrierImage = carriers[legs[outboundId][5]][0];
-                    var carrierImageR = carriers[legs[inboundId][5]][0];
-                    var carrierName = carriers[legs[outboundId][5]][1];
-                    var carrierNameR = carriers[legs[inboundId][5]][1];
-                } catch (err) {
-                    var carrierImage = 'http://s1.apideeplink.com/images/airlines/AA.png';
-                    var carrierImageR = 'http://s1.apideeplink.com/images/airlines/AA.png';
-                    var carrierName = 'American Airlines';
-                    var carrierNameR = 'American Airlines';
-                }
-                var originStops = legs[inboundId][6];
-                var destinationStops = legs[outboundId][6];
-                var outboundDuration = legs[outboundId][7] / 60;
-                var inboundDuration = legs[inboundId][7] / 60;
-
-                var pricingOptions = {};
-                //            flight.PricingOptions.map(function(option) {
-                //               pricingOptions = {agent: agents[option.Agents[0]], price: Math.round(option.Price * 100)/100, DeeplinkUrl: option.DeeplinkUrl}
-                //            });
-
-                var pricingOptions = {
-                    'agent': agents[flight.PricingOptions[0].Agents[0]],
-                    'price': service.currency + " " + Math.round(flight.PricingOptions[0].Price * 100) / 100,
-                    'DeeplinkUrl': flight.PricingOptions[0].DeeplinkUrl,
-                    priceNoCurrency: Math.round(flight.PricingOptions[0].Price * 100) / 100
-                }
-                service.loading = false;
-                var result = {
-                    _id: index,
-                    arrivalRDate: arrivalRDate,
-                    arrivalRTime: arrivalRTime,
-                    departureRTime: departureRTime,
-                    departureRDate: departureRDate,
-                    arrivalDate: arrivalDate,
-                    arrivalTime: arrivalTime,
-                    departureTime: departureTime,
-                    departureDate: departureDate,
-                    carrierName: carrierName,
-                    carrierNameR: carrierNameR,
-                    destination: places[destination],
-                    origin: places[origin],
-                    pricingOptions: pricingOptions,
-                    inboundDuration: Math.round(inboundDuration),
-                    outboundDuration: Math.round(outboundDuration),
-                    carrierImage: carrierImage,
-                    carrierImageR: carrierImageR,
-                    originStops: originStops.reduce(function(originStops, start) {
-                        originStops.push(places[start])
-                        return originStops;
-                    }, []),
-                    originStopsLength: originStops.length,
-                    destinationStops: destinationStops.reduce(function(destinationStops, start) {
-                        destinationStops.push(places[start]);
-                        return destinationStops;
-                    }, []),
-                    destinationStopsLength: destinationStops.length
-                };
-                originStops.reduce(function(originStops, start) {
-                    originStops.push(places[start])
-                    return originStops;
-                }, [])
-                return result;
-
+                return setFlightsObject(flight, index, mainObject);
             });
 
             setTimeout(showController, 50);
             if (id === 0) {
                 $http.get('/newTicket').then(function(result, err) {
-                    console.log(result);
                     service.journeyId = result.data;
-                    console.log(result.data)
                 }).catch(function(err) {
                     // you need to send a message to the admin to make sure the record is not lost
                     console.log(err);
@@ -246,7 +157,81 @@
             }
 
             service.flights.currentPage = service.flights[id];
-            console.log(new Date)
+        }
+
+        function setFlightsObject(flight, index, mainObject) {
+            var outboundId = flight.OutboundLegId;
+            var inboundId = flight.InboundLegId;
+            var legs = mainObject.legs;
+            var carriers = mainObject.carriers;
+            var places = mainObject.places;
+            var agents = mainObject.agents;
+
+            try {
+                var carrierImage = carriers[legs[outboundId][5]][0];
+                var carrierImageR = carriers[legs[inboundId][5]][0];
+                var carrierName = carriers[legs[outboundId][5]][1];
+                var carrierNameR = carriers[legs[inboundId][5]][1];
+            } catch (err) {
+                var carrierImage = 'http://s1.apideeplink.com/images/airlines/AA.png';
+                var carrierImageR = 'http://s1.apideeplink.com/images/airlines/AA.png';
+                var carrierName = 'American Airlines';
+                var carrierNameR = 'American Airlines';
+            }
+            try {
+                var carrierImage = carriers[legs[outboundId][5]][0];
+                var carrierImageR = carriers[legs[inboundId][5]][0];
+                var carrierName = carriers[legs[outboundId][5]][1];
+                var carrierNameR = carriers[legs[inboundId][5]][1];
+            } catch (err) {
+                var carrierImage = 'http://s1.apideeplink.com/images/airlines/AA.png';
+                var carrierImageR = 'http://s1.apideeplink.com/images/airlines/AA.png';
+                var carrierName = 'American Airlines';
+                var carrierNameR = 'American Airlines';
+            }
+
+            var pricingOptions = {
+                'agent': agents[flight.PricingOptions[0].Agents[0]],
+                'price': service.currency + " " + Math.round(flight.PricingOptions[0].Price * 100) / 100,
+                'DeeplinkUrl': flight.PricingOptions[0].DeeplinkUrl,
+                priceNoCurrency: Math.round(flight.PricingOptions[0].Price * 100) / 100
+            }
+
+            service.loading = false;
+            var originStops = legs[inboundId][6].reduce(function(originStops, start) {
+                originStops.push(places[start])
+                return originStops;
+            }, []);
+
+            var destionationStops = legs[outboundId][6].reduce(function(destinationStops, start) {
+                destinationStops.push(places[start]);
+                return destinationStops;
+            }, []);
+            var result = {
+                _id: index,
+                arrivalRDate: legs[inboundId][0].split('T')[0],
+                arrivalRTime: inboundId && legs[inboundId][0].split('T')[1].slice(0, 5),
+                departureRTime: inboundId && legs[inboundId][1].split('T')[1].slice(0, 5),
+                departureRDate: inboundId && legs[inboundId][1].split('T')[0],
+                arrivalDate: legs[outboundId][0].split('T')[0],
+                arrivalTime: legs[outboundId][0].split('T')[1].slice(0, 5),
+                departureTime: legs[outboundId][1].split('T')[1].slice(0, 5),
+                departureDate: legs[outboundId][1].split('T')[0],
+                carrierName: carrierName,
+                carrierNameR: carrierNameR,
+                destination: places[legs[outboundId][3]],
+                origin: places[legs[outboundId][4]],
+                pricingOptions: pricingOptions,
+                inboundDuration: Math.round(legs[inboundId][7] / 60),
+                outboundDuration: Math.round(legs[outboundId][7] / 60),
+                carrierImage: carrierImage,
+                carrierImageR: carrierImageR,
+                originStops: originStops,
+                originStopsLength: originStops.length,
+                destinationStops: destionationStops,
+                destinationStopsLength: destionationStops.length
+            };
+            return result;
         }
 
     }
